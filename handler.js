5de6595a99b2b7a7d81dec1255ea0d0271377cea
nadunindunil <nadun1indunil@gmail.com>
2016-08-29 19:43:51 +0530

    update function fixed

--- handler.js
+++ handler.js
@@ -1,17 +1,30 @@
 
 var DOC = require('dynamodb-doc');
 var docClient = new DOC.DynamoDB();
+var AWS = require("aws-sdk");
 var table = "interns";
 
 module.exports.postUser = function(event, context) {
   console.log(JSON.stringify(event, null, ' '));
+  var docClient = new AWS.DynamoDB.DocumentClient();
 
   var datetime = new Date().getTime().toString();
   var params = {};
   params.TableName = "interns";
   params.Item = {
-                  "id"  : event.body.id,
-                  "username" : event.body.username
+                  "id"  : event.id,
+                  "firstname" : event.firstname ,
+                  "lastname" : event.lastname ,
+                  "fullname" : event.fullname,
+                  "NIC" : event.NIC,
+                  "password" : event.password,
+                  "email" : event.email,
+                  "mobile" : event.mobile,
+                  "tel" : event.tel,
+                  "address" : event.address,
+                  "goals" : event.goals,
+                  "social" : event.social,
+                  "techs" : event.techs
                 };
 
   var pfunc = function(err, data) {
@@ -25,7 +38,52 @@ module.exports.postUser = function(event, context) {
 
   docClient.putItem(params, pfunc);
 };
+////////////////////////////////////////////////////////////////////////////////
+module.exports.updateUser = function(event, context) {
+  var docClient = new AWS.DynamoDB.DocumentClient()
 
+  console.log(JSON.stringify(event, null, ' '));
+
+
+  var params = {
+      TableName:"interns",
+      Key:{"id"  : event.id},
+      UpdateExpression: "set firstname = :f, lastname= :l, fullname= :fn, nic= :nic, email= :e, mobile= :m, tel= :t, address= :a, goals= :g, social= :s, techs= :t ",
+      ExpressionAttributeValues:{
+          ":f" : event.firstname,
+          ":l" : event.lastname,
+          ":fn" : event.fullname,
+          ":nic" : event.NIC,
+          ":e" : event.email,
+          ":m" : event.mobile,
+          ":t" : event.tel,
+          ":a" : event.address,
+          ":g" : event.goals,
+          ":s" : event.social,
+          ":t" : event.techs
+
+      },
+      ReturnValues:"UPDATED_NEW"
+
+  };
+
+  console.log("Updating the item...");
+
+
+  var pf = function(err, data) {
+      if (err) {
+          console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+          context.fail('ERROR: ' + err);
+      } else {
+          console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+          context.succeed('It worked!');
+      }
+  };
+
+  docClient.update(params,pf);
+
+};
+///////////////////////////////////////////////////////////////////////////////////
 module.exports.getUsers = function(event, context) {
 
   docClient.scan({
@@ -49,7 +107,7 @@ module.exports.getUsers = function(event, context) {
     }
   );
 };
-
+////////////////////////////////////////////////////////////////////////////////////////////////
 module.exports.getUser = function(event, context) {
 
   console.log(event.body.id);
